75751246975de8a34bb6d2ec4d001e18d50b1c1c
Nitika0914 <124388218+Nitika0914@users.noreply.github.com>
2023-07-13 23:24:32 +0530

    Added program for product of two numbers

---
+++ product.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    cout<<"Enter amount 1: ";
+    int amt1;
+    cin>>amt1;
+    
+    cout<<"Enter amount 2: ";
+    int amt2;
+    cin>>amt2;
+
+    int pro = amt1 * amt2;
+    cout<<"Total product is: ";
+    cout<<pro<<endl;
+    return 0;
+}
