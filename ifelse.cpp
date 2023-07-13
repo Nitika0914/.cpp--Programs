9b61115d2318115a6a6b30b25bc41becb76c545d
Nitika0914 <124388218+Nitika0914@users.noreply.github.com>
2023-07-13 23:45:23 +0530

    Added program for ifelse program

---
+++ ifelse.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    if(n>10){
+        cout<<"more than 10"<<endl;
+    } else{
+        cout<<"less than 10"<<endl;
+    }
+    return 0;
+}
