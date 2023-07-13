6d7208f9c050ba11fb8e8acd5a2e59969f2e6f92
Nitika0914 <124388218+Nitika0914@users.noreply.github.com>
2023-07-13 23:21:26 +0530

    Added program for sum of two numbers

---
+++ Sum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    cout<<"Enter amount 1: ";
+    int amt1;
+    cin>>amt1;
+    
+    cout<<"Enter amount 2: ";
+    int amt2;
+    cin>>amt2;
+
+    int sum = amt1 + amt2;
+    cout<<"Total amount is: ";
+    cout<<sum<<endl;
+    return 0;
+}
